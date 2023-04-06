d2ce5a51ce26901e73bfb2cfb15aecba1f9b2100
Nicolas V <nicolas.valdes.h@usach.cl>
2023-04-05 22:30:19 -0400

    Creación de proyecto
    Se crea el proyecto monolitico para mingeso usando postgreSQL

---
+++ mingeso/src/test/java/com/usach/mingeso/MingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usach.mingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
